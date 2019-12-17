337a3667fad21fa50ededdcb1ef00d26e8475a4c
kimheyougyung <dmlgus0607@naver.com>
2019-12-17 18:59:29 +0900

    Initial commit

---
+++ app/src/main/java/com/example/householdledgerex/home/HomeLayout.java
@@ -0,0 +1,23 @@
+package com.example.householdledgerex.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.householdledgerex.R;
+
+public class HomeLayout extends Fragment {
+    View v;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+      v = inflater.inflate(R.layout.home_layout, container, false);
+      return v;
+    }
+}
